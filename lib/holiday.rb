44be410a8d79d22ce8e4ac0efaaafd88cb9a62b3
Theo Weller <weller.theodor@gmail.com>
2019-02-28 16:09:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
 
 holiday_hash.each do |key, value|
-  puts "#{key.capitalize}"
+  puts "#{key.capitalize}:"
   value.each do |holiday, array|
     puts "#{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{array.join(", ")}"
 end
